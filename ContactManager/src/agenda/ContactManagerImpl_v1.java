96fe7af2aa0ca9797d2677c8dea667a6a1a59c47
ldupui01 <ludo.dupuis@gmail.com>
2014-02-06 15:53:14 +0000

    New version using interface for the list

--- ContactManager/src/agenda/ContactManagerImpl_v1.java
+++ ContactManager/src/agenda/ContactManagerImpl_v1.java
@@ -1,28 +1,135 @@
 package agenda;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
+import java.util.ArrayList;
 
-public class ContactManagerImpl implements ContactManager {
+public class ContactManagerImpl_v1 implements ContactManager {
+	private ArrayList<String[]> stringImport;
 	private Set<Contact> setContact;
+	private Set<ContactImpl> setContactImpl;
 	private Set<Meeting> setMeeting;
+	private Set<MeetingImpl> setMeetingImpl;
 	private Set<PastMeeting> setPastMeeting;
 	private Set<FutureMeeting> setFutureMeeting;
 	
-	public ContactManagerImpl(){
+	public ContactManagerImpl_v1(){
+		this.stringImport=null;
 		this.setMeeting = new HashSet<Meeting>();
+		this.setMeetingImpl = new HashSet();
 		this.setContact = new HashSet<Contact>();
+		this.setContactImpl = new HashSet();
 		this.setPastMeeting  = new HashSet<PastMeeting>();
 		this.setFutureMeeting  = new HashSet<FutureMeeting>();
 	}
-
-// *********************** TOOLS needed for method in the interface implementation ***************	
 	
-	public void Import(String fileName){
-		
+	public void  readTextFile(){
+		TextReader tr = new TextReader();
+		this.stringImport= tr.reader("..\\contacts.txt");
+		this.createSets(this.stringImport);
+	}
+	
+	public void createSets(ArrayList<String[]> al){
+		String[] strImport = null;
+		int uid = 0;
+		String name = null;
+		String note = null;
+		String date = null;
+		Calendar time = null;
+		String with = null;
+		for(int i = 0;i<al.size();i++){
+			strImport = al.get(i);
+			if (strImport[0] != null){
+				switch (strImport[4]){
+				case("Contact"):
+					uid = Integer.parseInt(strImport[0]);
+					name = strImport[1];
+					note = strImport[2];
+					Contact contact = new ContactImpl(uid,name,note);
+					this.setContact.add(contact);
+					ContactImpl contactImpl = new ContactImpl(uid,name,note);
+					this.setContactImpl.add(contactImpl);
+					break;
+				case("Meeting"):
+					uid = Integer.parseInt(strImport[0]);
+					date = strImport[1];
+					time = this.setDate(date);
+					with = strImport[2];
+					Set<Contact> setMeetingContact = this.setMeetingContact(with);
+					Meeting meeting = new MeetingImpl(uid, time, setMeetingContact);
+					this.setMeeting.add(meeting);
+					MeetingImpl meetingImpl = new MeetingImpl(uid, time, setMeetingContact);
+					this.setMeetingImpl.add(meetingImpl);
+					note = strImport[3];
+/*
+ * set future meeting and past meeting	HERE ... use date compare !!!				
+ */
+					break;
+				default:
+					break;
+				}
+			}
+		}
+
+	}
+	
+	public ContactImpl retriveContactImpl(int id, Set<ContactImpl> set){
+		ContactImpl ci = new ContactImpl(0,"","");
+		for (ContactImpl obj : set){
+			if (obj.equals(id))
+				ci = obj;
+		}
+		return ci;
+	}
+	
+	public Contact retriveContact(int id, Set<Contact> set){
+		Contact ci =  new ContactImpl(0,"","");
+		for (Contact obj : set){
+			if (obj.equals(id))
+				 ci = obj;
+		}
+		return ci;
+	}
+	
+	public Calendar setDate(String s){
+	    int year = Integer.parseInt(s.substring(6, 10));
+	    int month = (Integer.parseInt(s.substring(3, 4))) - 1; // -1 because the month start at 0 for January
+	    int date = Integer.parseInt(s.substring(0, 1));
+	    Calendar cal = Calendar.getInstance();
+	    cal.clear();
+	    cal.set(year, month, date);
+		return cal;
+	}
+	
+	public Calendar today(){
+		Calendar today =  Calendar.getInstance();
+		return today;
+	}
+	
+	public Set<Contact> setMeetingContact(String s){
+		Set<Contact> setMeetCont = new HashSet<Contact>();
+		setMeetCont.clear();
+		int contactId=-1;
+		int tagIndex = 0;
+		int tagEndIndex = 0;
+		boolean stop = false;
+		do {
+			tagEndIndex = s.indexOf(",",tagIndex);
+			if (tagEndIndex>0){
+				contactId = Integer.parseInt(s.substring(tagIndex, tagEndIndex));
+				tagIndex = tagEndIndex+1;
+				setMeetCont.add(this.retriveContact(contactId,this.setContact));
+			}else{
+				contactId = Integer.parseInt(s.substring(tagIndex, s.length()));
+				setMeetCont.add(this.retriveContact(contactId,this.setContact));
+				stop = true;
+			}
+		}while (!stop);
+		return setMeetCont;
 	}
 	
 	public int findId (String s){
@@ -32,9 +139,9 @@ public class ContactManagerImpl implements ContactManager {
 			do{
 				check = true;
 				id++;
-				Iterator<Contact> it = setContact.iterator();
+				Iterator<ContactImpl> it = setContactImpl.iterator();
 				while (it.hasNext()){
-					Contact obj = it.next();
+					ContactImpl obj = it.next();
 					if(obj.getId()==id){
 						check = false;
 					}
@@ -44,9 +151,9 @@ public class ContactManagerImpl implements ContactManager {
 			do{
 				check = true;
 				id++;
-				Iterator<Meeting> it = setMeeting.iterator();
+				Iterator<MeetingImpl> it = setMeetingImpl.iterator();
 				while (it.hasNext()){
-					Meeting obj = it.next();
+					MeetingImpl obj = it.next();
 					if(obj.getId()==id){
 						check = false;
 					}
@@ -99,13 +206,12 @@ public class ContactManagerImpl implements ContactManager {
 		Meeting meeting = new MeetingImpl(id, date, contacts);
 		this.setMeeting.add(meeting);
 		MeetingImpl meetingImpl = new MeetingImpl(id, date, contacts);
-		this.setMeeting.add(meetingImpl);
-		FutureMeeting futureMeetingImpl = new FutureMeetingImpl(id, date, contacts);
-		this.setFutureMeeting.add(futureMeetingImpl);
+		this.setMeetingImpl.add(meetingImpl);
+		FutureMeetingImpl futuremeetingImpl = new FutureMeetingImpl(id, date, contacts);
+		this.setFutureMeeting.add(futuremeetingImpl);
 		
 		return id;
 	}
-	
 
 	@Override
 	public PastMeeting getPastMeeting(int id) throws IllegalArgumentException {
@@ -130,7 +236,7 @@ public class ContactManagerImpl implements ContactManager {
 			return pm;
 		}
 	}
-	
+
 	@Override
 	public FutureMeeting getFutureMeeting(int id) throws IllegalArgumentException {
 		FutureMeeting fm = null;
@@ -172,15 +278,18 @@ public class ContactManagerImpl implements ContactManager {
 			return m;
 		}
 	}
-	
 
 	@Override
-	public List<Meeting> getFutureMeetingList(Contact contact) {
+	public List<Meeting> getFutureMeetingList(Contact contact) throws IllegalArgumentException{
 		// TODO Auto-generated method stub
+		/* create list empty 
+		 * then call the set future list 
+		 * check if contact in set contact of the meeting 
+		 * and add them to the list according to their index (to create as in past meeting)
+		 */
+		
 		return null;
 	}
-	
-	
 
 	@Override
 	public List<Meeting> getFutureMeetingList(Calendar date) {
@@ -198,7 +307,6 @@ public class ContactManagerImpl implements ContactManager {
 	public void addNewPastMeeting(Set<Contact> contacts, Calendar date,
 			String text) {
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
@@ -210,7 +318,6 @@ public class ContactManagerImpl implements ContactManager {
 	@Override
 	public void addNewContact(String name, String notes) {
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
